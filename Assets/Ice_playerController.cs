a9093341e9884fc56f5e9f640455e5210f978292
Lojaen <lojaenayash@gmail.com>
2026-02-26 04:32:30 +0300

    add player move

--- Assets/Ice_playerController.cs
+++ Assets/Ice_playerController.cs
@@ -1,49 +1,66 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
-public class Ice_playerController : MonoBehaviour       
+public class Ice_playerController : MonoBehaviour
 {
-   //components
-   Rigidbody myRB;
-   Transform myAvatar;
+    // Components
+    private Rigidbody myRB;
+    private Transform myAvatar;
 
-   //player movement variables
-   [SerializeField] InputAction WASD;
-   Vector2 movementInput;
-   [SerializeField] float movementSpeed;
+    // Input
+    [SerializeField] private InputAction WASD;
+    private Vector2 movementInput;
 
+    // Movement
+    [SerializeField] private float movementSpeed = 5f;
 
-   private void onEnable()
-   {
-       WASD.Enable();
-   }
+    // Store original scale
+    private Vector3 originalScale;
 
-    private void onDisable()
+    private void OnEnable()
     {
-         WASD.Disable();
+        WASD.Enable();
+    }
+
+    private void OnDisable()
+    {
+        WASD.Disable();
     }
 
     void Start()
     {
+        // Get Rigidbody
         myRB = GetComponent<Rigidbody>();
+
+        // Get avatar (child object)
         myAvatar = transform.GetChild(0);
-    }       
+
+        // Save original size
+        originalScale = myAvatar.localScale;
+    }
 
     void Update()
     {
+        // Read input
         movementInput = WASD.ReadValue<Vector2>();
 
-        if (movementInput.x !=0)
+        // Flip character without changing size
+        if (movementInput.x != 0)
         {
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+            myAvatar.localScale = new Vector3(
+                Mathf.Sign(movementInput.x) * Mathf.Abs(originalScale.x),
+                originalScale.y,
+                originalScale.z
+            );
         }
     }
 
     private void FixedUpdate()
     {
-        myRB.linearVelocity = movementInput * movementSpeed;
+        myRB.linearVelocity = new Vector3(
+            movementInput.x,
+            movementInput.y,
+            0
+        ) * movementSpeed;
     }
-
 }
